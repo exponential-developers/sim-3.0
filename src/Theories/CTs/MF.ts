089da9a929352650a1da05684e7c74e83d2c2a84
Vladyslav Babkin <vladiksonic@gmail.com>
2025-11-24 22:40:03 -0800

    updateMilestonesNoMS;

--- src/Theories/CTs/MF.ts
+++ src/Theories/CTs/MF.ts
@@ -59,7 +59,6 @@ class mfSim extends theoryClass<theory> {
   vtot: number;
   resets: number;
   stopReset: boolean;
-  prevMilestoneCount: number;
   resetBundle: resetBundle;
   goalBundle: resetBundle;
   goalBundleCost: number;
@@ -160,21 +159,12 @@ class mfSim extends theoryClass<theory> {
   getMilestonePriority(): number[] {
     return [0, 1, 2, 3, 4, 5];
   }
-  updateMilestones(): void {
-    const rho = Math.max(this.maxRho, this.lastPub);
-    let milestoneCount = binaryInsertionSearch(this.milestoneUnlocks, rho);
-    if(milestoneCount != this.prevMilestoneCount) {
-      this.prevMilestoneCount = milestoneCount;
-      const priority = this.getMilestonePriority();
-      this.milestones = new Array(this.milestonesMax.length).fill(0);
-      for (let i = 0; i < priority.length; i++) {
-        while (this.milestones[priority[i]] < this.milestonesMax[priority[i]] && milestoneCount > 0) {
-          this.milestones[priority[i]]++;
-          milestoneCount--;
-        }
-      }
+  updateMilestonesNoMS(): boolean {
+    const res = super.updateMilestonesNoMS();
+    if(res) {
       this.updateC();
     }
+    return res;
   }
 
   omegaexp(): number {
@@ -230,7 +220,6 @@ class mfSim extends theoryClass<theory> {
     this.forkOnC1 = false;
     this.milestoneUnlocks = [20, 50, 175, 225, 275, 325, 425, 475, 525];
     this.milestonesMax = [1, 1, 2, 2, 2, 1];
-    this.prevMilestoneCount = -1;
     this.variables =
     [
       new Variable({ name: "c1", cost: new FirstFreeCost(new ExponentialCost(10, 2)), valueScaling: new StepwisePowerSumValue(2, 7) }), // c1
@@ -256,7 +245,7 @@ class mfSim extends theoryClass<theory> {
     this.goalBundle = [0, 0, 0, 0];
     this.goalBundleCost = 0;
     this.bestRes = null;
-    this.updateMilestones();
+    this.updateMilestonesNoMS();
     this.resetParticle();
   }
   copyFrom(other: this) {
@@ -305,7 +294,7 @@ class mfSim extends theoryClass<theory> {
       if (!global.simulating) break;
       this.tick();
       this.updateSimStatus();
-      this.updateMilestones();
+      this.updateMilestonesNoMS();
       this.buyNormalVariables();
       // These checks are here for optimization:
       if (!this.stopReset && this.rho.value >= this.goalBundleCost + 0.0001) {
