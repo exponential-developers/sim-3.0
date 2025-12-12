9a0634b02ff1836be20e67e1ae94d008a9d9199f
Vladyslav Babkin <vladiksonic@gmail.com>
2025-12-11 20:36:33 -0800

    Speed up passive sim a tiny bit;

--- src/Theories/CTs/CSR2.ts
+++ src/Theories/CTs/CSR2.ts
@@ -18,7 +18,7 @@ type pubTable = {[key: string]: number};
 const lowboundsActive = [0.65, 0.15, 0.85, 0, 0];
 const highboundsActive = [1.45, 0.5, 1.8, 1.2, 1.2];
 
-const lowboundsPassive = [0.65, 0.15, 0.85, 0, 0];
+const lowboundsPassive = [1, 0.15, 1.35, 0, 0];
 const highboundsPassive = [2.85, 0.5, 3.3, 1.2, 1.2];
 
 class csr2Sim extends theoryClass<theory> {
