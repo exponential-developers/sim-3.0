9d6bdb24af170cc8261bfec650958fed18b1f63b
MathisSinet <mathis.sinet@gmail.com>
2026-03-26 16:30:42 +0100

    Fixed a bug with CSR2 pub tables near e600 tau

--- src/Theories/CTs/CSR2.ts
+++ src/Theories/CTs/CSR2.ts
@@ -230,12 +230,14 @@ class csr2Sim extends theoryClass<theory> {
         || this.recursionValue[1] === 0
       ) await this.buyVariablesFork();
       if (this.lastPub < 500) this.updateMilestones();
-      if (this.forcedPubRho == 1500 && this.maxRho >= 1495 && this.doContinuityFork) {
+      if (this.forcedPubRho == 1500 && this.maxRho >= 1497 && this.doContinuityFork) {
         this.doContinuityFork = false;
         const fork = this.copy();
         fork.forcedPubRho = Infinity;
         const res = await fork.simulate(this.getDataForCopy());
-        this.bestRes = getBestResult(this.bestRes, res);
+        if (res.pubRho > 1500) {
+          this.bestRes = getBestResult(this.bestRes, res);
+        }
       }
     }
     if (this.recursionValue[1] === 1 || this.strat !== "CSR2XL")
