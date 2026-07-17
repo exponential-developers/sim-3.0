a57b388681bd2f85b6406e411e44614a3d4a31ee
MathisSinet <mathis.sinet@gmail.com>
2026-07-17 22:12:22 +0200

    Updated BD

--- src/Theories/Unofficial-CTs/BD.ts
+++ src/Theories/Unofficial-CTs/BD.ts
@@ -15,7 +15,7 @@ export default async function bd(data: theoryData): Promise<simResult> {
 type theory = "BD";
 
 class bdSim extends theoryClass<theory> {
-  static SYMMETRY_STEP = 0.25;
+  static  SYMMETRY_STEP = 0.25;
   static LATE_BOOST_STEP = 0.05;
   static K_TIME_DIVISOR = 60;
 
@@ -89,15 +89,18 @@ class bdSim extends theoryClass<theory> {
       new Variable({ name: "a2", cost: new ExponentialCost(1e3, 9, true), valueScaling: new ExponentialValue() }),
       new Variable({ name: "b1", cost: new ExponentialCost(1e4, 0.95, true), valueScaling: new StepwisePowerSumValue() }),
       new Variable({ name: "b2", cost: new ExponentialCost(1e15, 11, true), valueScaling: new ExponentialValue() }),
-      new Variable({ name: "c1", cost: new ExponentialCost(50, 1.2, true), valueScaling: new StepwisePowerSumValue(2, 10, 1) }),
-      new Variable({ name: "c2", cost: new ExponentialCost(1e25, 12, true), valueScaling: new ExponentialValue() }),
+      new Variable({ name: "c1", cost: new ExponentialCost(50, 1.015, true), valueScaling: new StepwisePowerSumValue(2, 10, 1) }),
+      new Variable({ name: "c2", cost: new ExponentialCost(1e25, 10.15, true), valueScaling: new ExponentialValue() }),
       new Variable({ name: "n", cost: new ExponentialCost(20, 2.5615, true), valueScaling: new LinearValue(1, 1) })
     ];
     this.updateMilestones();
   }
   updateMilestones() {
+    const prev_ms = this.milestones.reduce((p, c) => p+c, 0);
     super.updateMilestones();
-    this.rowTermIsDirty = true;
+    const new_ms = this.milestones.reduce((p, c) => p+c, 0);
+    // Only update this if sum of milestones changed
+    if (new_ms > prev_ms) this.rowTermIsDirty = true;
   }
   async simulate(): Promise<simResult> {
     while (!this.endSimulation()) {
@@ -111,8 +114,19 @@ class bdSim extends theoryClass<theory> {
 
     return this.createResult();
   }
-  getBinomialCoefficient(row: number, position: number): number {
-    return getFactorial(row) - getFactorial(position) - getFactorial(row - position);
+  getBinomialCoefficient(n: number, k: number): number {
+    return getFactorial(n) - getFactorial(k) - getFactorial(n - k);
+  }
+  getPartialSum(n: number, k: number): number {
+    k = Math.min(k, n-k);
+    if (k == Math.floor(n/2)) return n * l10(2);
+    let c = 0;
+    let s = 0;
+    for (let i=1; i<=k; i++) {
+      c = c + l10(n-i+1) - l10(i);
+      s = add(s, c);
+    }
+    return s + l10(2);
   }
   getPolynomialExponent(): number {
     let result = 0.25 * this.milestones[3]; // 1
@@ -121,43 +135,48 @@ class bdSim extends theoryClass<theory> {
     return result;
   }
   updateRowTerm() {
-    let row = this.variables[6].value;
-    let target = Math.floor(this.variables[6].value / 2);
+    const n = this.variables[6].value;
+    const target = Math.floor(this.variables[6].value / 2);
 
-    let position = Math.min(target, Math.floor(10 ** this.k));
-    let coefficient = this.getBinomialCoefficient(row, position);
+    const k = Math.min(target, Math.floor(10 ** this.k));
 
-    let fullRowFraction = this.milestones[5] / this.milestonesMax[5];
+    const fullRowFraction = this.milestones[5] / this.milestonesMax[5];
 
     let baseTerm;
-    if (this.milestones[5] === 0) baseTerm = coefficient;
-    else if (this.milestones[5] === this.milestonesMax[5]) baseTerm = l10(2) * row;
-    else baseTerm = coefficient * (1 - fullRowFraction) + l10(2) * row * fullRowFraction;
+    if (this.milestones[5] === 0) 
+      baseTerm = this.getBinomialCoefficient(n, k);
+    else if (this.milestones[5] === this.milestonesMax[5]) 
+      baseTerm = this.getPartialSum(n, k);
+    else 
+      baseTerm = this.getBinomialCoefficient(n, k) * (1 - fullRowFraction) 
+      + this.getPartialSum(n, k) * fullRowFraction;
 
-    this.cachedRowTerm = baseTerm + this.getPolynomialExponent() * l10(1 + row);
+    this.cachedRowTerm = baseTerm + this.getPolynomialExponent() * l10(1 + n);
     this.rowTermIsDirty = false;
   }
   tick() {
-    let target = l10(Math.floor(this.variables[6].value / 2));
-    let dk = this.variables[4].value;
-    if (this.milestones[2] > 0) dk += this.variables[5].value;
-    if (this.milestones[4] > 0) dk += l10(10) * (bdSim.SYMMETRY_STEP * this.milestones[4]);
-    dk -= l10(bdSim.K_TIME_DIVISOR);
+    const target = l10(Math.floor(this.variables[6].value / 2));
+    const symmetrySpeed = bdSim.SYMMETRY_STEP * this.milestones[4];
+    const vb2 = this.milestones[1] > 0 ? this.variables[3].value : 0;
+    const vc2 = this.milestones[2] > 0 ? this.variables[5].value : 0;
+    const prev_k = this.k;
+
+    // Updates k
     if (this.k < target) {
-        this.k = add(this.k, l10(this.dt) + dk);
+        const dk = l10(this.dt) + vc2 + symmetrySpeed - l10(bdSim.K_TIME_DIVISOR);
+        this.k = add(this.k, dk);
         this.k = Math.min(this.k, target);
-        if (this.k < target) {
+        if (this.k > prev_k) {
             this.rowTermIsDirty = true;
         }
     }
-
+    // Updates cached rho term
     if (this.rowTermIsDirty) {
         this.updateRowTerm();
     }
 
-    let dq = this.variables[2].value;
-    if (this.milestones[1] > 0) dq += this.variables[3].value;
-    this.q = add(this.q, l10(this.dt) + dq + this.cachedRowTerm);
+    const variableTerm = (this.variables[2].value + vb2 + this.variables[4].value + vc2) * 0.5;
+    this.q = add(this.q, l10(this.dt) + variableTerm + this.cachedRowTerm);
 
     let drho = (1 + 0.05 * this.milestones[0]) * this.variables[0].value + this.variables[1].value;
     this.rho.add(l10(this.dt) + this.totMult + drho + this.q);
@@ -173,5 +192,8 @@ class bdSim extends theoryClass<theory> {
     let copySim = new bdSim(this.getDataForCopy());
     copySim.copyFrom(this);
     return copySim;
+  }
+  onVariablePurchased(id: number): void {
+    if (id == 6) this.rowTermIsDirty = true;
   }
 }
